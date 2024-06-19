615bfe3dfe6b9d460bd65f9aca2213b359a6d0f3
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-06-18 22:36:06 -0400

    feat: new endpoint for create users

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,56 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { ConflictException, Injectable } from '@nestjs/common'
+import { CreateAuthDto } from './dto/create-auth.dto'
+import { UpdateAuthDto } from './dto/update-auth.dto'
+import { PrismaService } from '../prisma/prisma.service'
+import { handleErrorExceptions } from '../common/handleErrorsExcepcions'
+import { hash } from 'bcrypt'
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(private readonly prisma: PrismaService) {}
+  async create(createAuthDto: CreateAuthDto) {
+    const password = await hash(createAuthDto.password, parseInt(process.env.SALT_ROUNDS))
+    createAuthDto.password = password
+    try {
+      const email = await this.findUserByEmail(createAuthDto.email)
+      if (email) throw new ConflictException('Email already used')
+
+      return this.prisma.user.create({
+        data: createAuthDto,
+      })
+    } catch (error) {
+      handleErrorExceptions(error)
+    }
+  }
+
+  async findUserByEmail(email: string) {
+    try {
+      return this.prisma.user.findUnique({
+        where: { email },
+        select: {
+          username: true,
+          email: true,
+          Role: true,
+        },
+      })
+    } catch (error) {
+      handleErrorExceptions(error)
+    }
   }
 
   findAll() {
-    return `This action returns all auth`;
+    return `This action returns all auth`
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} auth`;
+    return `This action returns a #${id} auth`
   }
 
   update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
+    return `This action updates a #${id} auth`
   }
 
   remove(id: number) {
-    return `This action removes a #${id} auth`;
+    return `This action removes a #${id} auth`
   }
 }
