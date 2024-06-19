56291d422d8521a5df7ff3044746c075352bf11f
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-06-18 22:37:26 -0400

    feat: use swagget documents and pipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { Logger, ValidationPipe } from '@nestjs/common'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('main')
+
+  const app = await NestFactory.create(AppModule)
+
+  app.setGlobalPrefix('api')
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  )
+
+  const config = new DocumentBuilder().setTitle('RestFull Api').setDescription('Barber Shop').setVersion('1.0').addBearerAuth().build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
+  await app.listen(process.env.PORT)
+  logger.log(`App running in port ${process.env.PORT}`)
 }
-bootstrap();
+bootstrap()
