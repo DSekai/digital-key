d4ac19e090217d9a00180259e9cab206472dc176
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-06-19 20:52:37 -0400

    feat: use cookieparser

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { Logger, ValidationPipe } from '@nestjs/common'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import * as cookieParser from 'cookie-parser'
 
 async function bootstrap() {
   const logger = new Logger('main')
@@ -9,6 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   app.setGlobalPrefix('api')
+  app.use(cookieParser())
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -16,7 +18,7 @@ async function bootstrap() {
     })
   )
 
-  const config = new DocumentBuilder().setTitle('RestFull Api').setDescription('Barber Shop').setVersion('1.0').addBearerAuth().build()
+  const config = new DocumentBuilder().setTitle('RestFull Api').setDescription('DigitalKey').setVersion('1.0').addBearerAuth().build()
 
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('api', app, document)
