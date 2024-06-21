50a3a31427284a6425f75df699fdb96202e9fead
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-06-20 21:54:10 -0400

    feat: exception

--- src/common/handleErrorsExcepcions.ts
+++ src/common/handleErrorsExcepcions.ts
@@ -1,9 +1,11 @@
-import { ConflictException, InternalServerErrorException, NotFoundException, UnauthorizedException } from "@nestjs/common"
+import { ConflictException, InternalServerErrorException, NotFoundException, UnauthorizedException } from '@nestjs/common'
+import { JsonWebTokenError } from 'jsonwebtoken'
 
 export const handleErrorExceptions = (error: unknown) => {
   if (error instanceof NotFoundException) throw new NotFoundException(error.message)
   if (error instanceof UnauthorizedException) throw new UnauthorizedException(error.message)
   if (error instanceof ConflictException) throw new ConflictException(error.message)
+  if (error instanceof JsonWebTokenError) throw new UnauthorizedException(error.message)
 
-  throw new InternalServerErrorException("An unexpected error occurred")
+  throw new InternalServerErrorException('An unexpected error occurred')
 }
