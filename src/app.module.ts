6c07c0cf000b6d8efdc2f5d1a229c9ccb28a77e0
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-06-20 21:54:33 -0400

    feat: new module import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { ProductModule } from './product/product.module'
 import { AuthModule } from './auth/auth.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [ProductModule, AuthModule],
+  imports: [ProductModule, AuthModule, CategoryModule],
   controllers: [],
   providers: [],
 })
