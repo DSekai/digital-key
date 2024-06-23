4a96ace7799df036eb48479d719b6490130a9d76
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-06-22 20:56:35 -0400

    chore: remove arguments

--- src/auth/dto/update-auth.dto.ts
+++ src/auth/dto/update-auth.dto.ts
@@ -1,15 +1,4 @@
-import { ApiProperty, PartialType } from '@nestjs/swagger'
+import { PartialType } from '@nestjs/swagger'
 import { CreateAuthDto } from './create-auth.dto'
-import { $Enums } from '@prisma/client'
-import { IsEnum } from 'class-validator'
 
-export class UpdateAuthDto extends PartialType(CreateAuthDto) {
-  @ApiProperty({
-    example: 'CLIENT | ADMIN | EMPLOYEE',
-    description: 'The role of the',
-    nullable: false,
-    minLength: 4,
-  })
-  @IsEnum($Enums.Role)
-  role: $Enums.Role
-}
+export class UpdateAuthDto extends PartialType(CreateAuthDto) {}
