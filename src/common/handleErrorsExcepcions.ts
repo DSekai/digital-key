842e804e5bb19b79442e45de64249676fa880c62
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-06-22 20:57:38 -0400

    feat: new handle error exceptions

--- src/common/handleErrorsExcepcions.ts
+++ src/common/handleErrorsExcepcions.ts
@@ -1,4 +1,5 @@
 import { ConflictException, InternalServerErrorException, NotFoundException, UnauthorizedException } from '@nestjs/common'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
 import { JsonWebTokenError } from 'jsonwebtoken'
 
 export const handleErrorExceptions = (error: unknown) => {
@@ -6,6 +7,7 @@ export const handleErrorExceptions = (error: unknown) => {
   if (error instanceof UnauthorizedException) throw new UnauthorizedException(error.message)
   if (error instanceof ConflictException) throw new ConflictException(error.message)
   if (error instanceof JsonWebTokenError) throw new UnauthorizedException(error.message)
+  if (error instanceof PrismaClientKnownRequestError && error.code === 'P2002') throw new ConflictException('Data already exist')
 
   throw new InternalServerErrorException('An unexpected error occurred')
 }
