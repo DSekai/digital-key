946f1a34192ad52b4c43e2a099b7b6dfdeb4a8f3
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-06-25 19:03:25 -0400

    feat: new enpoint for create and update

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,34 +1,45 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { ProductService } from './product.service';
-import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
+import { Controller, Get, Post, Body, Param, Delete, Patch } from '@nestjs/common'
+import { ProductService } from './product.service'
+import { CreateProductDto } from './dto/create-product.dto'
+import { ApiOperation, ApiTags } from '@nestjs/swagger'
+import { Auth } from 'src/auth/decorator'
+import { UpdateProductDto } from './dto/update-product.dto'
 
+@ApiTags('Product')
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
+  @ApiOperation({
+    description: 'This endpoint is for create new users',
+  })
   @Post()
+  @Auth('ADMIN', 'CLIENT')
   create(@Body() createProductDto: CreateProductDto) {
-    return this.productService.create(createProductDto);
+    return this.productService.create(createProductDto)
   }
 
   @Get()
   findAll() {
-    return this.productService.findAll();
+    return this.productService.findAll()
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.productService.findOne(+id);
+  @Get(':product')
+  findOne(@Param('product') product: string) {
+    return this.productService.findOne(product)
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productService.update(+id, updateProductDto);
+  @ApiOperation({
+    description: 'This endpoint is for update product',
+  })
+  @Patch()
+  @Auth('ADMIN', 'EMPLOYEE')
+  update(@Body() updateProductDto: UpdateProductDto) {
+    return this.productService.update(updateProductDto)
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.productService.remove(+id);
+    return this.productService.remove(+id)
   }
 }
