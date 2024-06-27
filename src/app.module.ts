4691ffc03f3723582b175095354c60edc8bb57c4
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-06-26 20:53:56 -0400

    feat: new module key for product

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { ProductModule } from './product/product.module'
 import { AuthModule } from './auth/auth.module';
 import { CategoryModule } from './category/category.module';
+import { KeyModule } from './key/key.module';
 
 @Module({
-  imports: [ProductModule, AuthModule, CategoryModule],
+  imports: [ProductModule, AuthModule, CategoryModule, KeyModule],
   controllers: [],
   providers: [],
 })
---
+++ src/key/dto/create-key.dto.ts
@@ -0,0 +1,11 @@
+// import { ApiProperty } from '@nestjs/swagger'
+
+export class CreateKeyDto {
+  // @ApiProperty({
+  //   example: 19.99,
+  //   description: 'The price of the product',
+  //   nullable: false,
+  // })
+  // @IsNumber()
+  // price: number
+}
