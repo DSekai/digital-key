6fa6e5cf6e9ae9a9a6f8ad173381fa14b0ea3b8b
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-06-29 21:07:47 -0400

    chore: export product service

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -7,5 +7,6 @@ import { PrismaModule } from 'src/prisma/prisma.module'
   controllers: [ProductController],
   providers: [ProductService],
   imports: [PrismaModule],
+  exports: [ProductService],
 })
 export class ProductModule {}
