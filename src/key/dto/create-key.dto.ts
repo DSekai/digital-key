15fd6f30f6e8b40e4eb23e15c25ffd6c5adb9f49
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-06-29 21:07:31 -0400

    feat: new dto for keys

--- src/key/dto/create-key.dto.ts
+++ src/key/dto/create-key.dto.ts
@@ -1,11 +1,34 @@
-// import { ApiProperty } from '@nestjs/swagger'
+import { ApiProperty } from '@nestjs/swagger'
+import { ArrayNotEmpty, IsArray, IsString, ValidateNested } from 'class-validator'
+import { Type } from 'class-transformer'
+
+class KeyDto {
+  @ApiProperty({
+    example: 'bdsDuakAK-dj12A',
+    description: 'The key of the product',
+    nullable: false,
+  })
+  @IsString()
+  key: string
+
+  @ApiProperty({
+    example: 'f47ac10b-58cc-4372-a567-0e02b2c3d479',
+    description: 'The id of the product',
+    nullable: false,
+  })
+  @IsString()
+  productID: string
+}
 
 export class CreateKeyDto {
-  // @ApiProperty({
-  //   example: 19.99,
-  //   description: 'The price of the product',
-  //   nullable: false,
-  // })
-  // @IsNumber()
-  // price: number
+  @ApiProperty({
+    type: [KeyDto],
+    description: 'Array of product Keys',
+    nullable: false,
+  })
+  @IsArray()
+  @ArrayNotEmpty({ message: 'The keys array should not be empty' })
+  @ValidateNested({ each: true })
+  @Type(() => KeyDto)
+  keys: KeyDto[]
 }
