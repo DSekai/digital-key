38fe7d0580b51625c2fcf12241cafc7d7c762689
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-07-02 02:00:29 -0400

    feat: new dto for key relations with users

--- src/key/dto/relation-key-user.dto.ts
+++ src/key/dto/relation-key-user.dto.ts
@@ -1,20 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger'
-import { IsBoolean, IsString } from 'class-validator'
+import { IsEmail, IsUUID } from 'class-validator'
 
-export class UpdateKeyDto {
+export class FindKeyUserDto {
   @ApiProperty({
     example: 'f47ac10b-58cc-4372-a567-0e02b2c3d479',
     description: 'The id of the product',
     nullable: false,
   })
-  @IsString()
-  id: string
+  @IsUUID()
+  keyID: string
 
   @ApiProperty({
-    example: 'true',
-    description: 'Is used the product',
+    example: 'test@test.com',
+    description: 'The email of the user',
     nullable: false,
   })
-  @IsBoolean()
-  isUsed: boolean
+  @IsEmail()
+  email: string
 }
